074ee1ccc31de9d58ac9a3e71576ea8af5250af8
Ubuntu <ubuntu@ip-172-31-36-140.us-east-2.compute.internal>
2026-03-25 08:03:02 +0000

    chatter back end

--- chatterd/handlers.py
+++ chatterd/handlers.py
@@ -1,5 +1,12 @@
+from dataclasses import dataclass
+from datetime import datetime
 from http.client import HTTPException
+from psycopg.errors import StringDataRightTruncation
+from pydantic_core import to_jsonable_python
+from typing import Optional
+from uuid import UUID
 import httpx
+import main
 from starlette.background import BackgroundTask
 from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse, Response, StreamingResponse
@@ -24,3 +31,37 @@ async def llmprompt(request):
     return StreamingResponse(response.aiter_raw(),
         media_type="application/x-ndjson",
         background=BackgroundTask(response.aclose))
+
+@dataclass
+class Chatt:
+    name: str
+    message: str
+
+async def getchatts(request):
+    try:
+        async with main.server.pool.connection() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute('SELECT name, message, id, time FROM chatts ORDER BY time ASC;')
+                return JSONResponse(to_jsonable_python(await cursor.fetchall()))
+    except Exception as err:
+        print(f'{err=}')
+        return JSONResponse(f'{type(err).__name__}: {str(err)}', status_code=500)
+
+async def postchatt(request):
+    try:
+        chatt = Chatt(**(await request.json()))
+    except Exception as err:
+        print(f'{err=}')
+        return JSONResponse(f'Unprocessable entity: {str(err)}', status_code=422)
+    try:
+        async with main.server.pool.connection() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute('INSERT INTO chatts (name, message, id) VALUES '
+                    '(%s, %s, gen_random_uuid());', (chatt.name, chatt.message))
+                return JSONResponse({})
+    except StringDataRightTruncation as err:
+        print(f'Message too long: {str(err)}')
+        return JSONResponse(f'Message too long: {str(err)}', status_code=400)
+    except Exception as err:
+        print(f'{err=}')
+        return JSONResponse(f'{type(err).__name__}: {str(err)}', status_code=500)
