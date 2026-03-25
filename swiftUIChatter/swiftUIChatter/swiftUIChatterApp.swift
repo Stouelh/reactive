eff779fcd1afce8aa1f826c1d020e74ccd14f479
Stouelh <124311326+Stouelh@users.noreply.github.com>
2026-03-25 04:57:46 -0400

    llmprompt SwiftUI front end

---
+++ swiftUIChatter/swiftUIChatter/swiftUIChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIChatterApp.swift
+//  swiftUIChatter
+//
+//  Created by Macbook AIr on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
